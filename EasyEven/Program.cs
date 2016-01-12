8ec99cde7094df106df1500dcc3060328980c17d
asangam <san.adh2010@gmail.com>
2016-01-12 18:37:01 +0545

    C# Program
    A program demonstrating the even number from 1 to 100

---
+++ EasyEven/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyEven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            for (i = 2; i <= 100; i += 2)
+                Console.Write("" + i + "\n");
+                Console.ReadLine();
+        }
+    }
+}
